b925c317038c7ecc7a3176c6395e4e79c11e8dda
prathmeshrajmane <prathmesh.rajmane1@gmail.com>
2021-12-30 19:29:46 +0530

    added jenkins crfs script

---
+++ jenkinscrfs.ps1
@@ -0,0 +1,25 @@
+$user = 'admin'
+$pass = 'redhat'
+
+# The header is the username and password concatenated together
+$pair = "$($user):$($pass)"
+# The combined credentials are converted to Base 64
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+# The base 64 credentials are then prefixed with "Basic"
+$basicAuthValue = "Basic $encodedCreds"
+# This is passed in the "Authorization" header
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+# Make a request to get a crumb. This will be returned as JSON
+$json = Invoke-WebRequest -Uri 'http://localhost:9096/crumbIssuer/api/json' -Headers $Headers
+# Parse the JSON so we can get the value we need
+$parsedJson = $json | ConvertFrom-Json
+# See the value of the crumb
+Write-Host "The Jenkins crumb is $($parsedJson.crumb)"
+# Extract the crumb filed from the returned json, and assign it to the "Jenkins-Crumb" header
+$BuildHeaders = @{
+    "Jenkins-Crumb" = $parsedJson.crumb
+    Authorization = $basicAuthValue
+}
+Invoke-WebRequest -Uri "http://localhost:9096/job/Shared%20Library/build" -Headers $BuildHeaders -Method Post
